edbe6775813a23cf5a2ee86e61c0a3e200c69e2f
Emanoel Pedro <emanoelpedro791@gmail.com>
2023-12-26 00:28:55 -0300

    My First Commit

---
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class AdminDashboardController extends Controller
+{
+    public function __construct() {
+        $this->middleware(function ($request, $next) {
+            if(Auth::user()->getPermissionsViaRoles()->isEmpty()) {
+             return redirect()->route('site.home');
+         }
+            return $next($request);
+        });
+    }
+
+    public function home() 
+    {
+        $user = User::find(Auth::id());
+        // Auth::user()->assignRole('admin');
+        $permissions =(array)Auth::user()->getPermissionsViaRoles()->all();
+        foreach($permissions as $permission){
+            var_dump($permission->name);
+        }
+        return View('admin.dashboard');
+    }
+}
